aef41d2865193c084e2bd85135696d289b456d6f
YevaMV <eva280988@gmail.com>
2022-08-25 16:00:16 +0400

    sending Responses

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  // console.log(req.url, req.headers, req.method);
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>First Page</title></head>');
+  res.write('<body><h1>Hi from Node.js Server!!!</h1></body>');
+  res.write('</html>');
 });
 
 server.listen(3000);
